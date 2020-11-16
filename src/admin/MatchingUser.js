1bc0cfb5f92d4fc83dab1fb94e8d949a336aa947
jiahuilum <jiahuilum@gmail.com>
2020-11-16 21:10:52 +0800

    add back stars

--- src/admin/MatchingUser.js
+++ src/admin/MatchingUser.js
@@ -202,7 +202,12 @@ const destroyFavorites = () => {
              Filter: "",
              accessor: (text, i) => 
              <> <Link to={`/student/${text._id}`} target="_blank">{text.name} </Link>  <br/>
-              </> ,
+             { text.inJapan === true ? <span className="badge bg-red">日本在住 </span>: null }  <br/>
+
+             {text.importantDetails} <br/>
+
+             { text.ratings === 0 ? null: <b style={{fontSize: "16px"}}>{text.ratings} </b>}
+             </> ,
              id: "name"
            },
 
